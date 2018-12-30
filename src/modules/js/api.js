2df1e6abeba2de7ce5271f2c7d9712334a08408c
sixiaofeng <sxf754872493@gmail.com>
2018-12-30 23:23:36 +0800

    首页热门商品列表加载，底部导航静态

---
+++ src/modules/js/api.js
@@ -0,0 +1,17 @@
+let url = {
+  hostLists: '/index/hotLists'
+}
+
+// 开发环境和真实环境切换
+
+let host = 'http://rap2api.taobao.org/app/mock/7058'
+
+
+
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {
+    url[key] = host + url[key]
+  }
+}
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,7 +2,51 @@ import 'css/common.css'
 import './index.css'
 import Vue from 'vue'
 
+import axios from 'axios'
+import url from 'js/api.js'
+
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll)
+
+import foot from 'components/Foot.vue'
 
 new Vue({
   el: '#app',
+  data: {
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
+  },
+  components:{
+    foot,
+  },
+  methods: {
+    getData(){
+      if(this.allLoaded) return
+      this.loading = true
+      axios.get(url.hostLists,{
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res=>{
+        let currentLists = res.data.lists
+        // 判断所有数据是否已经加载完毕
+        if(currentLists.length < this.pageSize){
+          this.allLoaded = true
+        }
+        if(this.lists){
+          this.lists = this.lists.concat(currentLists)
+        }else{
+          // 第一次请求数据，初始化
+          this.lists = currentLists
+        }
+        this.pageNum++
+        this.loading = false
+      })
+    }
+  },
+  created(){
+    this.getData()
+  }
 })
